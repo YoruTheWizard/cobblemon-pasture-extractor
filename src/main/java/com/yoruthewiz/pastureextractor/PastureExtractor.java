6bbe1acbc8b67e40df491d4a55cf513334d84971
Álvaro Renan <yoruthewizard@outlook.com>
2026-03-20 12:01:02 -0300

    feat: set project base

---
+++ src/main/java/com/yoruthewiz/pastureextractor/PastureExtractor.java
@@ -0,0 +1,36 @@
+package com.yoruthewiz.pastureextractor;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(PastureExtractor.MOD_ID)
+public class PastureExtractor {
+    public static final String MOD_ID = "pastureextractor";
+    private static final Logger LOGGER = LoggerFactory.getLogger(PastureExtractor.class);
+
+    public PastureExtractor(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
